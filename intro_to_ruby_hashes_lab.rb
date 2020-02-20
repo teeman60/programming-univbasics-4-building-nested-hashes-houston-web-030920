23bcb29f3b656c84a437ea8824d65d317b466caf
Taiye Salami <taiye41@gmail.com>
2020-02-20 09:44:24 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,6 +25,12 @@ def monopoly_with_third_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
+  base = {
+	  :railroads => {
+	    :rent_in_dollars => 4
+	  
+	}
+   }
 end
 
 def monopoly_with_fourth_tier
