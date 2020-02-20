0ba4a102dff57ed9dd54e45a29b45e5168381bc7
Taiye Salami <taiye41@gmail.com>
2020-02-20 09:35:36 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,7 +1,10 @@
 def base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
-	{name: "Taiye Salami"}
+	base = {
+	  :name => "Taiye Salami"
+	  
+	}
 end
 
 def monopoly_with_second_tier
@@ -9,10 +12,14 @@ def monopoly_with_second_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
-  {
-    railroads: {:first => "Southern Rail"}
+  base = {
+	  :name => "Taiye Salami"
+	  
+	}
+  # {
+  #   railroads: {:first => "Southern Rail"}
     
-  }
+  # }
 end
 
 def monopoly_with_third_tier
