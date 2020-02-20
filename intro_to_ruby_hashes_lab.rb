526404af1dfea0025c018caa52be69dd1efb66ac
Taiye Salami <taiye41@gmail.com>
2020-02-20 11:00:08 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -59,10 +59,8 @@ def monopoly_with_fourth_tier
 	    },
 	
 	    :names => {
-	      :reading_railroad => {
-	        :mortgage_value => "$100"
-	        
-	      },
+	      :reading_railroad => 
+	        :mortgage_value => "$100",
 	      :pennsylvania_railroad => {},
 	      :b_and_o_railroad => {},
 	      :shortline_railroad => {}
