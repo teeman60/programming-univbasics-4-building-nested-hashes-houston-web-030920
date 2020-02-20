c5c192aa15cf4efc98dc6f2bc1409b4da4394266
Taiye Salami <taiye41@gmail.com>
2020-02-20 10:47:30 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -30,7 +30,7 @@ def monopoly_with_third_tier
 	    :rent_in_dollars => {
 	      :one_piece_owned => 25,
 	      :two_pieces_owned => 50,
-	      :key_three => "three",
+	      :three_pieces_owned => 100,
 	      :key_four => "four"
 	    },
 	
